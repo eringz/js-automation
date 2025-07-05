b52a320929d1936334dae7da8c4249203e699948
unknown <ron.garcia.santos@gmail.com>
2025-07-05 15:44:12 +0800

    doing the task 2

--- homework1.js
+++ homework1.js
@@ -1,4 +1,5 @@
-// Task 1
+console.log("\t\t\tTask 1");
+console.log()
 console.log(`I start to practice "JavaScript" today, and I like it.`);
 console.log(`The secret of getting ahead is getting started.`);
 console.log(`"Don't limit yourself.`);
@@ -6,3 +7,8 @@ console.log(`Invest in uour dreams. Grind now. Shine later.`);
 console.log(`Its not the load that breaks you down, its the way you carry it.`);
 console.log(`The hard days are what make you stronger.`);
 console.log(`You can waste your lives drawing lines. or you can live your life crossing them.`);
+
+console.log()
+console.log("\t\t\tTask 2");
+console.log()
+console.log("\tJavaScript is a high-level programming language primarily used to\n build web applications. It is not limited to the web; it can also be\n used for the backend development with technologies like Node.js.\n\n\tIt is open source and has a huge community support, which means\n there are pleanty of resources and support available for learning. It\n uses a syntax like other programming languages and easy to learn.");
