843d79a410d2138bc9df0f902dae5a3bbefb250b
unknown <ron.garcia.santos@gmail.com>
2025-07-05 13:05:24 +0800

    task 1 done

---
+++ homework1.js
@@ -0,0 +1,8 @@
+// Task 1
+console.log(`I start to practice "JavaScript" today, and I like it.`);
+console.log(`The secret of getting ahead is getting started.`);
+console.log(`"Don't limit yourself.`);
+console.log(`Invest in uour dreams. Grind now. Shine later.`);
+console.log(`Its not the load that breaks you down, its the way you carry it.`);
+console.log(`The hard days are what make you stronger.`);
+console.log(`You can waste your lives drawing lines. or you can live your life crossing them.`);
