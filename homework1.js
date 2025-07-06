97bfd5673287c37979359c66e53ca8fa41cdee0d
unknown <ron.garcia.santos@gmail.com>
2025-07-06 16:22:31 +0800

    completion of homework 1.

--- homework1.js
+++ homework1.js
@@ -7,8 +7,76 @@ console.log(`Invest in uour dreams. Grind now. Shine later.`);
 console.log(`Its not the load that breaks you down, its the way you carry it.`);
 console.log(`The hard days are what make you stronger.`);
 console.log(`You can waste your lives drawing lines. or you can live your life crossing them.`);
-
 console.log()
+
 console.log("\t\t\tTask 2");
 console.log()
 console.log("\tJavaScript is a high-level programming language primarily used to\n build web applications. It is not limited to the web; it can also be\n used for the backend development with technologies like Node.js.\n\n\tIt is open source and has a huge community support, which means\n there are pleanty of resources and support available for learning. It\n uses a syntax like other programming languages and easy to learn.");
+console.log();
+
+console.log("\t\t\tTask 3");
+const myAge = 36;
+const myFavoriteNumber = 17;
+const myHeight = 1.70;
+const myWeight = 70;
+const myFavoriteLetter = "D";
+
+console.log(myAge);
+console.log(myFavoriteNumber);
+console.log(myHeight);
+console.log(myWeight);
+console.log(myFavoriteLetter);
+
+
+console.log("\t\t\tTask 4");
+const num1 = 25, num2 = 35;
+console.log(`Test data:\n${num1}, ${num2}\n\nExpected Output:\nThe sum of the numbers = ${num1 + num2}`);
+console.log();
+
+console.log("\t\t\tTask 5");
+const n1 = 5, n2 = 7;
+console.log(`Test data:\n${n1}, ${n2}\n\nExpected Output:\nThe product of the numbers = ${n1 * n2}`);
+console.log();
+
+console.log("\t\t\tTask 6");
+const number1 = 24, number2 = 10;
+console.log(`Test data:\n${number1}, ${number2}\n\nExpected Output:\nThe sum of the numbers is = ${number1 + number2}\nThe product of the numbers is = ${number1 * number2}\nThe substraction of the numbers is = ${number1 - number2}\nThe division of the numbers is = ${number1 / number2}\nThe remainder of the numbers is = ${number1 % number2}`);
+console.log();
+
+console.log("\t\t\tTask 7");
+const i1 = 7, i2 = 11;
+console.log(`Test data:\n${i1}, ${i2}\n\nExpected Output:\nThe average of the numbers is: ${(i1 + i2) / 2}`);
+console.log();
+
+
+console.log("\t\t\tTask 8");
+const a1 = 6, a2 = 10, a3 = 12, a4 = 15, a5 = 17;
+console.log(`Test data:\n${a1}, ${a2}, ${a3}, ${a4}, ${a5}\n\nExpected Output:\nThe average of the numbers is: ${(a1 + a2 + a3 + a4 + a5) / 5}`);
+console.log();
+
+console.log("\t\t\tTask 9");
+const b1 = 5, b2 = 6, b3 = 10;
+console.log(`Test data:\n${b1}, ${b2}, ${b3}\n\nExpected Output:\nThe ${b1} multiplied by ${b1} is = ${b1 * b1}\nThe ${b2} multiplied by ${b2} is = ${b2 * b2}\nThe ${b3} multiplied by ${b3} is = ${b3 * b3}\n`);
+console.log();
+
+console.log("\t\t\tTask 10");
+const side = 7;
+console.log(`Test data:\n${side}\n\nExpected Output:\nThe perimeter of the square is = ${4 * side}\nThe area of the square is = ${side * side}`);
+console.log();
+
+console.log("\t\t\tTask 11");
+const favBook = "JS Algorithms & Data Structures";
+const favColor = "Blue";
+const favNumber = 7;
+console.log(`Expected Output:\nThe favorite book is ${favBook}\nThe favorite color is ${favColor}\nThe favorite number is ${favNumber}`);
+console.log();
+
+console.log("\t\t\tTask 12");
+const firstName = "John";
+const lastName = "Doe";
+const age = 45;
+const emailAddress = "johndoe@gmail.com";
+const phoneNumber = "(123) 123 1234";
+const address = "123 St Chicago IL 12345";
+console.log(`Expected Output:(it should exactly be same as below paragraph)\n\n\tUser who joined this program is ${firstName + " " + lastName}. ${firstName}'s age is ${age}.\n${firstName}'s email address is ${emailAddress}, phone number is\n${phoneNumber}, and address is ${address}.`);
+
