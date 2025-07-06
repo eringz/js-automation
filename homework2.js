c0bec36e17308177b959ffe7f4bb59663a5aaa62
unknown <ron.garcia.santos@gmail.com>
2025-07-06 21:33:06 +0800

    homework 2 and 3 setup

---
+++ homework2.js
@@ -0,0 +1,80 @@
+console.log("Task 1\n");
+/**
+ * Convert given Strings below to nuber data types and find thier sum, product, division,subtraction, remainder and exponentiation 
+ * let str1 = "5", str2 = "2";
+ * Expected results:
+    The sum of 5 and 2 is = 7
+    The product of 5 and 2 is = 10
+    The division of 5 and 2 is = 2.5
+    The subtraction of 5 and 2 is = 3
+    The remainder of 5 and 2 is = 1
+    The exponentiation of 5 and 2 is 25
+ */
+
+console.log("Task 2\n");
+/**
+ * Convert given Strings below to number data types and find the greatest and smalles values and find the average and absolute difference of them.
+ * let s1 = "200", s2 = "-50";
+ * Expected results:
+    The greatest value is = 200
+    The smallest value is = -50
+    The average is = 75
+    The absolute difference is = 250
+ */
+
+console.log("Task 3\n");
+/**
+ * Write a program that generates 2 random numbers between 1 and 50 (both 1 and 50 are included). Then, find the absolute difference of these numbers.
+ * Test Data:
+    5, 27
+ * Expected Output:
+    The aboslute difference between number is = 22
+ */
+
+console.log("Task 4\n");
+/**
+ * Write a program that generates 5 random numbers between 1 and 50 (both 1 and 50 are included). Then, find the max and min of these numbers.
+ * Test Data:
+    3, 15, 45, 20, 25
+ * Expected Output:
+    The max value = 45
+    The min value = 3   
+ */
+
+console.log("Task 5\n");
+/**
+ * Write a program that generates 3 random numbers between 50 and 100 both included.
+ * First print the numbers
+ * Then, find the sum of these numbers
+ * Test Data:
+    The number 1 = 55
+    The number 2 = 67
+    The number 3 = 90
+    The sum of numbers is = 212
+ */
+
+console.log("Task 6\n");
+/**
+ * Write a program that generates 3 random numbers between 1 and 100 (1 and 100 are included) and find if all the numbers are more than 25.
+ * Print true if all numbers are greater than 25.
+ * Print false if any of the number is less than or equals 25
+ */
+
+console.log("Task 7\n");
+/**
+ * Assume you are given a name let name = "David";
+    1. Print out the length of the name
+    2. Find the first character in the name and print it
+    3. Find the last characteer in the name and print it
+    4. Find the first 3 characters in the name and print them
+    5. Find the last 3 characters in the name and print them
+ * Expected Output:
+    The length of the name is = 5
+    The first character in the name is = D
+    The last character in the name is = d
+    The first 3 characters in the name are = Dav
+    The last 3 characters in the name are = vid
+ */
+
+
+
---
+++ homework3.js
@@ -0,0 +1,58 @@
+console.log("Task-1\n");
+/**
+ * Write a program that generates 3 random numbers between 1 to 100 (1 and 100 are included)
+ * Print true if the average of the random numbers is greater or equals 50.
+ * Print false if the average fo the random numbers is less than 50.
+ * Test data 1:
+    20, 70, 25
+ * Expected Output 1:
+    false
+ * Test data 2:
+    65, 80, 90
+ * Expected Output 2:
+    true
+ */
+
+console.log("Task-2\n");
+/**
+ * Write a program that generates 3 random numbers between 1 to 3 (1 and 3 are included)
+    - If all numbers are different, then print "NO MATCH"
+    - If any of those 2 numbers are equal, then prin "DOUBLE MATCH"
+    - If all 3 numbers are equal, then print "TRIPLE MATCH"
+ * Test data 1:
+    3, 3, 3
+ * Expected Output 1:
+    TRIPLE MATCH
+ * Test data 2:
+    1, 3, 3
+ * Expected Output 2:
+    DOUBLE MATCH
+ * Test data 3:
+    1, 2, 3
+ * Expected Output 3:
+    NO MATCH
+ * Test data 4:
+    1, 1, 2
+ * Expected Output 4:
+    DOUBLE MATCH
+ */
+
+console.log("Task-3\n");
+/**
+ * Write a function named as hasA() which takes a string word as an argument and returns true 
+    if given string ha a character "a" or "A", and false otherwise when invoked.
+ * NOTE: Assume you will not be given an empty word.
+ * Examples: 
+    hasA("Tech")        -> false
+    hasA("Global")      -> true
+    hasA("")            -> false
+    hasA("Apple")       -> true
+ */
+
+
+console.log("Task-4\n");
+/**
+ * Write a function names as doubleOrTripleWord() which takes a string word as an argument and returns the given word back 
+    tripled if the string length is even or doubled if the string le
+ * 
+ */
