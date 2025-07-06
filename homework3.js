68c5a6319d14111aa849f5eba9296fdfdc10ab14
unknown <ron.garcia.santos@gmail.com>
2025-07-06 23:36:56 +0800

    updating the setup for homework 3

--- homework3.js
+++ homework3.js
@@ -53,6 +53,86 @@ console.log("Task-3\n");
 console.log("Task-4\n");
 /**
  * Write a function names as doubleOrTripleWord() which takes a string word as an argument and returns the given word back 
-    tripled if the string length is even or doubled if the string le
- * 
+    tripled if the string length is even or doubled if the string length is odd when invoked.
+ * Examples:
+    doubleOrTripleWord("Tech")          -> "TechTechTech"
+    doubleOrTripleWord("Apple")         -> "AppleApple"
+    doubleOrTripleWord(" ")             -> " "
+    doubleOrTripleWord("1")             -> "11"
+    doubleOrTripleWord("22")            -> "222222"
+ */
+
+console.log("Task-5\n")
+/**
+ * Write a function named as firstWord() which takes a string word as an argument and returns the first word from the given string when invoked.
+ * NOTE: Return empty string if the given string does not have any word.
+ * Examples:
+    firstWord("Hello World")            -> "Hello"
+    firstWord("I like JavaScript")      -> "I"
+    firstWord("Hello")                  -> "Hello"
+    firstWord("")                       -> ""
+    firstWord(" ")                      -> ""
+ */
+
+console.log("Task-6\n");
+/**
+ * Write a function named as lastWord() which takes a string word as an argument and returns the last word from the given string when invoked.
+ * NOTE: Return empty string if the given string does not have any word.
+ * Examples:
+    lastWord("Hello World")             -> "World"
+    lastWord("I like JavaScript")       -> "JavaScript"
+    lastWord("Hello")                   -> "Hello"
+    lastWord("")                        -> ""
+    lastWord(" ")                       -> ""
+ */
+
+console.log("Task-7\n");
+/**
+ * Write a function named as firstlastWord() which takes a string word as an argument and returns the first and last words from the given string when invoked.
+ * NOTE: Return empty string if the given string does not have any word.
+ * Examples:
+    firstLastWord("Hello World")        -> "HelloWorld"
+    firstLastWord("I like JavaScript")  -> "IJavaScript"
+    firstLastWord("Hello")              -> "HelloHello"
+    firstLastWord("")                   -> ""
+    firstLastWord(" ")                  -> ""
+ */
+
+console.log("Task-8\n");
+/**
+ * Write a function names as startVowel() which takes a string word as an argument and returns true if given string starts with a vowel, and false otherwise when invoked.
+ * NOTE: Vowel letters: a, e, i, o, u, A, E, I, O, U
+ * Examples:
+    startVowel("Hello")                 -> false
+    startVowel("Apple")                 -> true 
+    startVowel("orange")                -> true
+    startVowel("")                      -> false
+    startVowel(" ")                     -> false
+    startVowel("123")                   -> false
+ */
+
+console.log("Task-9\n");
+/**
+ * Write a function names as swap4() which takes a string word as an argument and returns the string back with its first and last 4 characters swapped when invoked.
+ * NOTE: Return empty string if the given string does not have 8 or more characters.
+ * Examples:
+    swap4("abc")                        -> ""
+    swap4("JavaScript")                 -> "riptScJava"
+    swap4("TechGlobal")                 -> "obalGITech"
+    swap4("")                           -> ""
+    swap4(" ")                          -> ""
+    swap4("Apple")                      -> ""
+ */
+
+console.log("Task-10\n");
+/**
+ * Write a function names as swapFirstLastWord() which takes a string word as an argument and returns the string back with its first and last words swapped when invoked.
+ * NOTE: Return empty string if the given string does not have 2 or more words.
+ * Examples:
+    swapFirstLastWord("Hello World")        -> "World Hello"
+    swapFirstLastWord("I like JavaScript")  -> "JavaScript like I"
+    swapFirstLastWord("foo bar foo bar")    -> "bar bar foo foo"
+    swapFirstLastWord("")                   -> ""
+    swapFirstLastWord("Hello")              -> ""
+    swapFirstLastWord("Hello ")              -> ""
  */
